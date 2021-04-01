0c70dec1a9347e6f869c5a976e0578883557b561
Coffeemag <andrey@coffeemag.net>
2021-04-01 21:02:56 +0300

    modified:   routes/web.php 	resources/views/phplearn.blade.php 	resources/views/phplearn2.blade.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/php', function () {
+    return view('phplearn');
+});
+
+Route::get('/php2', function () {
+    return view('phplearn2');
+});
